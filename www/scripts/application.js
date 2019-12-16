f7d1a8010a796978ae83f10c97c7815394d636ab
Agustin Bignu <32014773+abignu@users.noreply.github.com>
2019-12-16 19:05:14 +0100

    Scripts para detectar clicks en pantalla offline

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -1,30 +1,102 @@
 define(["require", "exports"], function (require, exports) {
-    // For an introduction to the Blank template, see the following documentation:
-    // http://go.microsoft.com/fwlink/?LinkID=397705
-    // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-    // and then run "window.location.reload()" in the JavaScript Console.
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var ServerURL = "https://bk.kubikoplaygrounds.com/games/bk/";
+    var xmlhttp = new XMLHttpRequest();
+    var uuid = '';
+    var onlineStatus = "off";
     function initialize() {
         document.addEventListener('deviceready', onDeviceReady, false);
     }
     exports.initialize = initialize;
     function onDeviceReady() {
-        document.addEventListener('pause', onPause, false);
-        document.addEventListener('resume', onResume, false);
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        /* var parentElement = document.getElementById('deviceready');
-         var listeningElement = parentElement.querySelector('.listening');
-         var receivedElement = parentElement.querySelector('.received');
-         listeningElement.setAttribute('style', 'display:none;');
-         receivedElement.setAttribute('style', 'display:block;');*/
-        Kiosk.setKiosEnabled(true);
+        console.log("device ready!");
+        InitMessageEventListener();
     }
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
+    var cargadoReceptorMessages = false;
+    setTimeout(InitMessageEventListener, 10000);
+    function InitMessageEventListener() {
+        if (cargadoReceptorMessages)
+            return;
+        cargadoReceptorMessages = true;
+        StatusBar.hide();
+        if (navigator.connection.type == Connection.NONE)
+            SetOfflineMode();
+        else
+            SetOnlineMode();
+        uuid = device.uuid;
+        SendToServer({ uuid: device.uuid, device: device });
+        window.addEventListener('online', SetOnlineMode, false);
+        window.addEventListener('offline', SetOfflineMode, false);
+        window.addEventListener("message", ReceiveMessage, false);
     }
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
+    function limpiarDebug() {
+        GetById("debug").innerText = '';
+    }
+    function MsgDebug(msg) {
+        GetById("debug").innerText = msg;
+        setTimeout(limpiarDebug, 2000);
+    }
+    function SendToServer(data) {
+        xmlhttp.open("POST", ServerURL + "api.php");
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xmlhttp.send(JSON.stringify(data));
+    }
+    function SetOnlineMode() {
+        onlineStatus = "on";
+        GetById("offline").style.display = "none";
+        GetById("games").style.display = "block";
+    }
+    function SetOfflineMode() {
+        onlineStatus = "off";
+        GetById("offline").style.display = "block";
+        GetById("games").style.display = "none";
+    }
+    function GetById(id) {
+        return document.getElementById(id);
+    }
+    function ReceiveMessage(e) {
+        var msg = e.data.data;
+        switch (msg) {
+            case 'kiosko':
+                KioskPlugin.isInKiosk(function (isInKiosk) {
+                    MsgDebug('kiosko: ' + isInKiosk.toString());
+                });
+                break;
+            case 'launcher':
+                KioskPlugin.isSetAsLauncher(function (isLauncher) {
+                    MsgDebug('launcher ' + isLauncher.toString());
+                });
+                break;
+            case 'quit':
+                Quit();
+                break;
+            default:
+                SendToServer({ uuid: uuid, game: msg });
+                break;
+        }
+    }
+    var quitTimeout, vecesQuit = 0;
+    function tryQuit() {
+        if (onlineStatus == "on")
+            return;
+        clearTimeout(quitTimeout);
+        {
+            vecesQuit++;
+            if (vecesQuit == 5)
+                Quit();
+        }
+        quitTimeout = setTimeout(limpiarQuit, 1500);
+    }
+    function limpiarQuit() {
+        vecesQuit = 0;
+    }
+    function Quit() {
+        var clave = window.prompt("Por favor, ingresa tu clave: ");
+        if (clave == "kbk123") {
+            alert("ATENCIÓN: se va a alterar el modo Kiosko!");
+            KioskPlugin.exitKiosk();
+        }
     }
 });
 //# sourceMappingURL=application.js.map
